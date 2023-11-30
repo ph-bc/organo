1f22d62eefb89905a165b287055fe6bd347747f6
Pedro <pedro_berchior@outlook.com>
2023-11-30 13:28:20 -0300

    first commit

---
+++ src/components/Banner/index.js
@@ -0,0 +1,14 @@
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <header className="banner">
+      <img
+        src="/images/banner.png"
+        alt="banner principal da Página do Organo"
+      ></img>
+    </header>
+  );
+}
+
+export default Banner;
